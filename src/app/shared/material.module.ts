bf5a26046822499a3e17ad6f0d7eccbeb0a6f535
George Stamati <george.stamati@yahoo.com>
2024-05-23 13:19:01 +0300

    add explanation and steps for running the app

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -3,6 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   imports: [
@@ -10,12 +12,16 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
+    MatCardModule,
+    MatDividerModule,
   ],
   exports: [
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
+    MatCardModule,
+    MatDividerModule,
   ],
 })
 export class MaterialModule {}
