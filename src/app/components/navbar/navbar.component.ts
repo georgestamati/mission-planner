8f01d4abbfbcd79becd38dee34c9fbb0ce0b9368
George Stamati <george.stamati@yahoo.com>
2024-05-23 12:51:14 +0300

    change share to button

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,19 +5,25 @@ import { filter, map } from 'rxjs';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent implements OnInit {
   title = '';
 
-  constructor(private router: Router, private route: ActivatedRoute){}
-  
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
   ngOnInit(): void {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-      map(() => this.route.snapshot.firstChild?.data)
-    ).subscribe(data => {
-      this.title = data? data['title'] : '';
-    });
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map(() => this.route.snapshot.firstChild?.data)
+      )
+      .subscribe((data) => {
+        this.title = data ? data['title'] : '';
+      });
+  }
+
+  goToGithubPage() {
+    window.location.href = 'https://github.com/georgestamati/mission-planner';
   }
 }
