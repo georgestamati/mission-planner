e520e9fa6d9cd5635563a7dda06bde7f19174101
George Stamati <george.stamati@yahoo.com>
2024-05-23 12:46:29 +0300

    implement navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PlannerComponent } from './components/planner/planner.component';
+import { PlayerComponent } from './components/player/player.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+      title: 'Mission Planner',
+    },
+  },
+  {
+    path: 'planner',
+    component: PlannerComponent,
+    data: {
+      title: 'Planner',
+    },
+  },
+  {
+    path: 'player',
+    component: PlayerComponent,
+    data: {
+      title: 'Player',
+    },
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent implements OnInit {
+  title = '';
+
+  constructor(private router: Router, private route: ActivatedRoute){}
+  
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.route.snapshot.firstChild?.data)
+    ).subscribe(data => {
+      this.title = data? data['title'] : '';
+    });
+  }
+}
